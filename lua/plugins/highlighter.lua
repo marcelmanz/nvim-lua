5054cb476d9c546875d915dad798e18ecff8cc09
themarcel <code@marcel.cool>
2026-07-30 14:49:14 +0200

    fix plugin url

--- lua/plugins/highlighter.lua
+++ lua/plugins/highlighter.lua
@@ -1,7 +1,7 @@
-local local_path = vim.fn.expand "~/clones/own/highlighter.nvim"
+local local_path = vim.fn.expand "~/clones/own/h"
 
 return {
-	"marcelmanz/highlighter.nvim",
+	"https://codeberg.org/marcelmanz/highlighter.nvim.git",
 	dir = vim.fn.isdirectory(local_path) == 1 and local_path or nil,
 	config = function()
 		require("highlighter").setup {
