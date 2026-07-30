ceb125724ead927f3925a9e63a8a5a6f6d489510
themarcel <code@marcel.cool>
2026-07-30 09:39:03 +0200

    fallback to remote plugin

--- lua/plugins/highlighter.lua
+++ lua/plugins/highlighter.lua
@@ -1,6 +1,8 @@
+local local_path = vim.fn.expand "~/clones/own/highlighter.nvim"
+
 return {
-	-- "marcelmanz/highlighter.nvim",
-	dir = "~/clones/own/highlighter.nvim/",
+	"marcelmanz/highlighter.nvim",
+	dir = vim.fn.isdirectory(local_path) == 1 and local_path or nil,
 	config = function()
 		require("highlighter").setup {
 			highlights = {
