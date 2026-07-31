f43ce36e508c691dd5ad4bce981aea1a1af49f09
marcelmanz <code@marcel.cool>
2026-08-01 01:41:56 +0200

    update configs

--- lua/plugins/highlighter.lua
+++ lua/plugins/highlighter.lua
@@ -5,10 +5,10 @@ return {
 	dir = vim.fn.isdirectory(local_path) == 1 and local_path or nil,
 	config = function()
 		require("highlighter").setup {
-			highlights = {
-				HighlighterDefault = "#FF8800",
-				TextReview = "#00CCFF",
-			},
+			fuzzy_default = false,
+			-- highlights = {
+			-- 	TextReview = "Visual"
+			-- },
 		}
 	end,
 }
