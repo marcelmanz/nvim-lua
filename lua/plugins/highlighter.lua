1e510e8370bb2e74591758dada6e1681e52a810b
marcelmanz <code@marcel.cool>
2026-08-19 17:01:05 +0200

    updatw

--- lua/plugins/highlighter.lua
+++ lua/plugins/highlighter.lua
@@ -2,7 +2,7 @@ local local_path = vim.fn.expand "~/clones/own/highlighter.nvim/"
 
 return {
 	"https://codeberg.org/marcelmanz/highlighter.nvim.git",
-	-- dir = vim.fn.isdirectory(local_path) == 1 and local_path or nil,
+	dir = vim.fn.isdirectory(local_path) == 1 and local_path or nil,
 	config = function()
 		require("highlighter").setup {
 			fuzzy_default = false,
